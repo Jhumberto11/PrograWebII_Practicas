2a283c9ffa9245d3c4c62c134ff9c855f2ab6ce3
JonathanH2004 <imosa253@gmail.com>
2026-07-28 15:56:30 -0600

    Gestion de autores terminada

--- Biblio/Controllers/AutoresController.cs
+++ Biblio/Controllers/AutoresController.cs
@@ -6,9 +6,7 @@ namespace Biblio.Controllers
 {
     public class AutoresController : Controller
     {
-        public IActionResult Index()
-        {
-            List<Autor> autores = new List<Autor>()
+        private static List<Autor> _autores = new List<Autor>()
             {
                  new Autor()
                  {
@@ -18,7 +16,7 @@ namespace Biblio.Controllers
                      Country = "Colombia",
                      Birthdate = new DateOnly(1927, 3, 6)
                  },
-                
+
                  new Autor()
                  {
                      Id = 2,
@@ -28,7 +26,7 @@ namespace Biblio.Controllers
                      Birthdate = new DateOnly(1942, 8, 2),
                      IsActive = false
                  },
-                
+
                  new Autor()
                  {
                      Id = 3,
@@ -37,7 +35,7 @@ namespace Biblio.Controllers
                      Country = "Argentina",
                      Birthdate = new DateOnly(1899, 8, 24)
                  },
-                
+
                  new Autor()
                  {
                      Id = 4,
@@ -46,7 +44,7 @@ namespace Biblio.Controllers
                      Country = "España",
                      Birthdate = new DateOnly(1547, 9, 29)
                  },
-                
+
                  new Autor()
                  {
                      Id = 5,
@@ -56,12 +54,114 @@ namespace Biblio.Controllers
                      Birthdate = new DateOnly(1936, 3, 28)
                  }
             };
-
-
-
-
-            ViewBag.Autores = autores;
+        public IActionResult Index()
+        { 
+            ViewBag.Autores = _autores;
             return View();
         }
+
+        public IActionResult Details(int id)
+        {
+            var autor = _autores.FirstOrDefault(x => x.Id == id);
+            if(autor == null)
+            {
+                return NotFound();
+            }
+            return View(autor);
+
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryTokenAttribute]
+        public IActionResult AddAutor(Autor autor)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(autor);
+            }
+
+            if (_autores.Any())
+            {
+                autor.Id = _autores.Max(a => a.Id) + 1;
+            }
+            else
+            {
+                autor.Id = 1;
+            }
+
+            _autores.Add(autor);
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult EditView(int id)
+        {
+            var autor  = _autores.FirstOrDefault(a=> a.Id == id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+            return View(autor); 
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult EditAutor(Autor autorUpdated)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(autorUpdated);
+            }
+
+
+            var autor = _autores.FirstOrDefault(a => a.Id == autorUpdated.Id);
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            autor.Name = autorUpdated.Name;
+            autor.Birthdate = autorUpdated.Birthdate;
+            autor.Lastname = autorUpdated.Lastname;
+            autor.Country = autorUpdated.Country;
+
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public IActionResult DeleteAutor(int id)
+        {
+            var autor = _autores.FirstOrDefault(a => a.Id == id);
+
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            return View(autor);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteAutorConfirmed(int id)
+        {
+            var autor = _autores.FirstOrDefault(a => a.Id == id);
+
+            if (autor == null)
+            {
+                return NotFound();
+            }
+
+            _autores.Remove(autor);
+
+            return RedirectToAction(nameof(Index));
+        }
     }
+
 }
--- Biblio/Models/Autor.cs
+++ Biblio/Models/Autor.cs
@@ -1,12 +1,19 @@
-﻿namespace Biblio.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Biblio.Models
 {
     public class Autor
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
         public string Lastname { get; set; }
+        [StringLength(50)]
         public string Country { get; set; }
+        [DataType(DataType.Date)]
         public DateOnly Birthdate { get; set; }
+        
         public bool IsActive { get; set; } = true;
 
     }
