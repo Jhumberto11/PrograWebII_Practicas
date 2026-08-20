87687b5f63ae84232436d8f368d8d7d109386201
JonathanH2004 <imosa253@gmail.com>
2026-08-19 21:52:57 -0600

    semana5

---
+++ Biblio/Models/InfoCard.cs
@@ -0,0 +1,8 @@
+﻿namespace Biblio.Models
+{
+    public class InfoCard
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ Biblio/Models/ServiceCard.cs
@@ -0,0 +1,12 @@
+﻿namespace Biblio.Models
+{
+    public class ServiceCard
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+        public string Text { get; set; }
+
+        public string? ImageUrl { get; set; }
+    }
+}
